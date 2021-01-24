c23a8d13bf84d496fd55d9d3e814d3fd0d6558cf
ninvit <ninvit@gmail.com>
2021-01-24 15:03:03 -0300

    Creating SafeAreaView and FlatList

--- App.js
+++ App.js
@@ -1,1 +1,23 @@
-export default () => {}
+export default () => {
+
+    return (
+        <SafeAreaView style={styles.container}>
+          <Text style={styles.header}>SWAPI People's List</Text>
+          <FlatList
+            style={styles.list}
+            data={people}
+            keyExtractor={(item) => item.url}
+            renderItem={({ item }) => (
+              <View>
+                <TouchableOpacity onPress={() => pressHandler(item)}>
+                  <Text style={styles.listItem}>{item.name}</Text>
+                </TouchableOpacity>
+              </View>
+            )}
+            onEndReached={loadMore}
+            onEndReachedThreshold={0.1}
+          />
+        </SafeAreaView>
+      );
+    };
+}
